6aea1e7914d7bfec35552ddf2b05f19930f04e80
duboisfacu <facundubois@hotmail.com>
2021-07-12 22:59:11 -0300

    ♥

---
+++ Clases/Clase 01/C1 - Ejercicio 03.js
@@ -0,0 +1,5 @@
+//Escribe un programa que le pida al usuario ingresar un número, luego le pide un segundo número, e imprima en la consola la suma de los dos números que ingresó el usuario.
+
+let num1 = parseInt(prompt('Ingrese el primer número:'))
+let num2 = parseInt(prompt('Ingrese el segundo número:'))
+console.log(num1 + num2)
---
+++ Clases/Clase 01/C1 - Ejercicio 04.js
@@ -0,0 +1,7 @@
+//Escribe un programa que le pida al usuario su año de nacimiento e imprima su edad actual en la consola con la frase "Tienes X años". Por ejemplo, asumiendo que el año actual es 2020 y el usuario ingresa 2000, el programa debe imprimir en la consola:
+//Tienes 20 años
+
+let currentYear = parseInt(prompt('Año actual:'))
+let bornYear = parseInt(prompt('Ingrese su año de nacimiento:'))
+let age = currentYear - bornYear
+console.log(`Tienes ${age} años`)
